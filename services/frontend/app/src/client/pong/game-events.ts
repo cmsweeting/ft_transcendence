34233284f4cd9c091c8e617989bf5b72d3f9e7b2
Charlotte Sweeting <charlottesweeting@email>
2026-03-11 16:30:00 +0100

    chore: switched to azerty

--- services/frontend/app/src/client/pong/game-events.ts
+++ services/frontend/app/src/client/pong/game-events.ts
@@ -11,9 +11,9 @@ export function addMessEvent(game: Game, ws: WebSocket) {
 
 export function createKeyEvent(keys: keysObj, local: boolean, horizontal: boolean, isKeyDown: boolean) {
 	const keyMap = {
-		w: ['w', 'W'],
+		w: ['z', 'Z'],
 		s: ['s', 'S'],
-		a: horizontal ? ['a', 'A'] : [],
+		a: horizontal ? ['q', 'Q'] : [],
 		d: horizontal ? ['d', 'D'] : [],
 		ArrowUp: local ? ['ArrowUp'] : [],
 		ArrowDown: local ? ['ArrowDown'] : [],
